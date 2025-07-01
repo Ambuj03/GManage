c2c868c4e013ab402ec00c62456a26aec377fd54
Ambuj03 <mishraambuj8269@gmail.com>
2025-07-01 12:56:19 +0530

    Feat: Added email operation apis

--- gmail_app/urls.py
+++ gmail_app/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import GmailEmailListView, GmailEmailMetadataView, GmailSearchView, GmailLabelsView
 
 urlpatterns = [
-    # JWT realted URLs
+    # JWT realted apis
     path("auth/register/",  UserRegistrationView.as_view(), name="user_register"),
     path('auth/login/', UserLoginView.as_view(), name = 'user_login'),
     path('auth/logout/', UserLogoutView.as_view(), name = 'user_logout'),
@@ -18,13 +18,15 @@ urlpatterns = [
     path('auth/google/status/', GoogleTokenStatusView.as_view(), name='google_token_status'),
     path('auth/google/revoke/', GoogleTokenRevokeView.as_view(), name='google_token_revoke'),
 
-    #Gmail connectivity test views
+    #Gmail connectivity test apis
     path('gmail/connectivity/', GmailConnectivityTestView.as_view(), name='gmail_connectivity'),
 
-    #Gmail operations related views
+    #Gmail operations related apis
     path('gmail/emails/', GmailEmailListView.as_view(), name='gmail_emails'),
     path('gmail/emails/metadata/', GmailEmailMetadataView.as_view(), name='gmail_email_metadata'),
     path('gmail/search/', GmailSearchView.as_view(), name='gmail_search'),
     path('gmail/labels/', GmailLabelsView.as_view(), name='gmail_labels'),
+
+    #Email operations relaeted apis
 
 ]
